74b5e09d54749ed72fb94dfc11652d1de69d2213
Lucas <lhbelfanti@gmail.com>
2024-11-30 02:23:03 -0300

    ⚙️ Added iTerm2 profile json. Added command to execute powerlevel10k

--- apps/install.sh
+++ apps/install.sh
@@ -2,6 +2,9 @@
 
 echo "Installing Apps..."
 
+echo "Coin Tick"
 unzip ./apps/CoinTick.app.zip
-
 cp -R "./Coin Tick.app" /Applications
+
+echo "Configuring iTerm2 powerlevel10k theme"
+p10k configure
